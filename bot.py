16e75430a0800a79d6f294e003801a16bed0947d
unknown <rubinzack40@gmail.com>
2020-10-28 03:57:19 -0400

    added !week command to pull info from every week

--- bot.py
+++ bot.py
@@ -240,17 +240,32 @@ async def on_message(message):
 
 ################ ################ ################ ################ ################ ################ 
     if message.content.startswith('!games'):
-        embed1 = discord.Embed(title='Games from the Week', color=0xA750DE)
+        embed1 = discord.Embed(title=schedule.week_element.text, color=0xA750DE)
         for i in schedule.games():
             if i[0] in schedule.teamRegions:
                 if ('am' != i[2][-2:]) and ('pm' != i[2][-2:]):
                     embed1.add_field(name=('{0} vs {1}'.format(i[0], i[1])), value='Result: {0}'.format(i[2]), inline=True)
                 else:
                     embed1.add_field(name=('{0} vs {1}'.format(i[0], i[1])), value='Time: {0}'.format(i[2]), inline=True)
-
             
         await message.channel.send(embed=embed1)
 ################ ################ ################ ################ ################ ################ 
+
+
+################ ################ ################ ################ ################ ################ 
+    if message.content.startswith('!week'):
+        msg = str(message.content).split()
+        weekNum = msg[-1]
+        embed1 = discord.Embed(title='NFL Schedule - Week {0}'.format(weekNum), color=0xA750DE)
+        for i in schedule.weeks(weekNum):
+            if ('am' != i[2][-2:]) and ('pm' != i[2][-2:]):
+                embed1.add_field(name=('{0} vs {1}'.format(i[0], i[1])), value='Result: {0}'.format(i[2]), inline=True)
+            else:
+                embed1.add_field(name=('{0} vs {1}'.format(i[0], i[1])), value='Time: {0}'.format(i[2]), inline=True)
+       
+        await message.channel.send(embed=embed1)
+################ ################ ################ ################ ################ ################ 
+
 
 
 ### need to set up auto update ###
--- schedule.py
+++ schedule.py
@@ -1,5 +1,7 @@
+import sqlite3
 import requests, re
 from bs4 import BeautifulSoup
+
 
 teamRegions = ['Arizona', 'Atlanta', 'Baltimore', 'Buffalo', 'Carolina', 'Chicago', 
                'Cincinnati', 'Clevland', 'Dallas', 'Denver', 'Detroit', 'Green Bay',
@@ -9,12 +11,13 @@ teamRegions = ['Arizona', 'Atlanta', 'Baltimore', 'Buffalo', 'Carolina', 'Chicag
                'Tampa Bay', 'Tennessee', 'Washington'  
 ]
 
+url = 'https://www.cbssports.com/nfl/schedule/'
+page = requests.get(url)
+soup = BeautifulSoup(page.content, 'html.parser')
+
+week_element = soup.find(id='PageTitle-header')
 
 def games():
-    url = 'https://www.cbssports.com/nfl/schedule/'
-    page = requests.get(url)
-    soup = BeautifulSoup(page.content, 'html.parser')
-    
     matchup_elems = soup.find_all(class_="TableBase-bodyTd")
 
     # gameLeaders = soup.find_all(class_= '?') 
@@ -29,7 +32,28 @@ def games():
     n = 0
     bigList = [matchupsList[x:x+3] for x in range(0, len(matchupsList), 3)]
 
-    # print(bigList)
     return bigList
 
-print(games())
+
+def weeks(x):
+    url2 = 'https://www.cbssports.com/nfl/schedule/2020/regular/{0}/'.format(x)
+    page2 = requests.get(url2)
+    soup2 = BeautifulSoup(page2.content, 'html.parser')
+        
+    matchup_elems = soup2.find_all(class_="TableBase-bodyTd")
+
+    # gameLeaders = soup.find_all(class_= '?') 
+
+    matchupsList = []
+
+    for i in matchup_elems:
+        matchupsList.append(' '.join(i.text.split()))
+
+    numOfGamesForTheWeek = len(matchupsList)/3
+
+    n = 0
+    bigList = [matchupsList[x:x+3] for x in range(0, len(matchupsList), 3)]
+
+
+    return bigList        
+
